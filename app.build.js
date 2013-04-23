74220950c1f5f8af8d610ada90ed11740d6c0662
takuhito-h <bluesbug@gmail.com>
2013-04-23 21:50:56 +0900

    二つのファイルをアップデート

--- app.build.js
+++ app.build.js
@@ -3,6 +3,11 @@
     appDir: ".",
     baseUrl: "js",
     dir: "../optimized-project",
+    paths: {
+        jQuery    : "lib/jquery-1.8.3.min",
+        modernizr : "lib/modernizr-2.6.2.min",
+        myLibrary : "lib/myLibrary"
+    },
     modules: [
         {
             name: "script"
--- js/lib/myLibrary.js
+++ js/lib/myLibrary.js
@@ -1,8 +1,10 @@
-/*
+/**
  *  myLibrary.js
- *  
- *  Copyright(c) 2012, Takuhito Hihara
- *  https://github.com/takuhito-h/myLibrary.js
+ *  --------------
+ *  @author: Takuhito Hihara
+ *  @since: 2012
+ *  @url https://github.com/takuhito-h/myLibrary.js
+ *  --------------
  *  
  *  Licensed under the MIT license:
  *  http://www.opensource.org/licenses/MIT
@@ -14,7 +16,7 @@
 ----------------------------------------------------------------------*/
 if(!window.console){
     window.console = {
-        log: function(){}
+        log : function(){}
     };
 }
 
@@ -44,16 +46,17 @@ if(!window.console){
             ----------------------------------------------------------------------*/
             zeroPadding: function(originInt, zeroVolume){
 
-                var resultVal = null,
-                    zeroString = "",
-                    originInt = parseInt(originInt, 10);
+                var resultVal  = null,
+                    zeroString = "";
 
+                originInt = parseInt(originInt, 10);
+
+                // 指定した桁数のゼロと元の数字を足した後、sliceで不要なゼロを削る
                 for(var i = 1; i < zeroVolume; i++){
                     zeroString = "0" + zeroString;
                 }
 
-                // 指定した桁数のゼロと元の数字を足した後、sliceで不要なゼロを削る
-                resultVal = (zeroString + originInt).slice(-1 * zeroVolume)
+                resultVal = (zeroString + originInt).slice(-1 * zeroVolume);
 
                 return resultVal;
                 
@@ -111,16 +114,18 @@ if(!window.console){
     ----------------------------------------------------------------------*/
     $.fn.fadeInDispatch = function(speed, func){
         return this.each(function(){
-            var self = this;
+            var fadeThis = this;
+
             if(!$.support.opacity){
                 $.when($(this).show(0, func)).then(function(){
-                    self.style.removeAttribute("filter");
+                    fadeThis.style.removeAttribute("filter");
                 });
             }else if($.iOSJudge()){
                 $(this).show(0, func);
             }else{
                 $(this).fadeIn(speed, func);
             }
+
         });
     };
 
@@ -137,6 +142,56 @@ if(!window.console){
         });
     };
 
+    /*--------------------------------------------------------------------------------------
+        jQueryプラグイン：リンクのフォーカス時、リンク要素を沈ませるようにする。
+        $.setLinkFocusSink();
+    --------------------------------------------------------------------------------------*/
+    $.setLinkFocusSink = function(){
+
+        $("body").on("mousedown", "a", function(){
+            var $this = $(this);
+
+            /*--------------------------------------------------------------------------------------
+                positionがstaticか、top or bottomの値が設定されてないか判定
+            --------------------------------------------------------------------------------------*/
+            if($this.css("position") === "static" || ($this.css("top") === "auto" && $this.css("bottom") === "auto")){
+                $this.addClass("focusLink");
+            }else{
+
+                /*--------------------------------------------------------------------------------------
+                    topが設定されてなかったらbottomが設定されているのでbottomの値を変更、
+                    bottomが設定されてなかったらtopが設定されているのでtopの値を設定
+                --------------------------------------------------------------------------------------*/
+                if($this.css("top") === "auto"){
+                    $.data($this, "linkFocusChangeVal", "bottom");
+                    $this.css("bottom", "-=1");
+                }else{
+                    $.data($this, "linkFocusChangeVal", "top");
+                    $this.css("top", "+=1");
+                }
+
+            }
+        }).on("mouseup", "a", function(){
+            var $this = $(this);
+
+            if($this.hasClass("focusLink")){
+                $this.removeClass("focusLink");
+            }else{
+
+                /*--------------------------------------------------------------------------------------
+                    data属性のlinkFocusChangeValがtopの値になっているか判定
+                --------------------------------------------------------------------------------------*/
+                if($.data($this, "linkFocusChangeVal") === "top"){
+                    $this.css("top", "");
+                }else{
+                    $this.css("bottom", "");
+                }
+                
+            }
+        });
+
+    };
+
     /*----------------------------------------------------------------------
         backgroundPositionを取得したり、設定したりする。
     ----------------------------------------------------------------------*/
@@ -144,30 +199,29 @@ if(!window.console){
 
         // 取得メソッド
         get: {
-            $ele: null,
-            bgPosDef: null,
-            bgPos: [],
+            $ele     : null,
+            bgPosDef : null,
+            bgPos    : [],
 
-            getVal: function(){
+            _getVal: function(){
 
                 this.bgPosDef = $(this.$ele).css("backgroundPosition");
 
                 // firefoxの場合backgroundPositionで取得する。
                 if(this.bgPosDef){
                     this.bgPosDef = this.bgPosDef.split(" ");
-                    this.bgPos["x"] = this.bgPosDef[0];
-                    this.bgPos["y"] = this.bgPosDef[1];  
+                    this.bgPos.x  = this.bgPosDef[0];
+                    this.bgPos.y  = this.bgPosDef[1];  
                 }else{
-                    this.bgPos["x"] = $(this.$ele).css("backgroundPositionX");
-                    this.bgPos["y"] = $(this.$ele).css("backgroundPositionY"); 
+                    this.bgPos.x = $(this.$ele).css("backgroundPositionX");
+                    this.bgPos.y = $(this.$ele).css("backgroundPositionY"); 
                 }
                 
             },
 
             run: function($ele){
                 this.$ele = $ele;
-
-                this.getVal();
+                this._getVal();
 
                 return this.bgPos;
             }
@@ -176,23 +230,23 @@ if(!window.console){
 
         // 設定メソッド
         set: {
-            $ele: null,
-            data: null,
-            dataEnableFlag: "",
+            $ele           : null,
+            data           : null,
+            dataEnableFlag : "",
 
             // データに単位を追加する
-            dataAddUnit: function(){
+            _dataAddUnit: function(){
                 var self = this;
 
-                $.each(this.data, function(i, num){
+                $.each(this.data, $.proxy(function(i, num){
                     // データに単位がついてない場合、単位をpxに
                     if(String(num).match(/^\d+$/)){
-                        self.data[i] = num + "px";
+                        this.data[i] = num + "px";
                     }
-                });
+                }, this));
             },
 
-            judgeXY: function(){
+            _judgeXY: function(){
                 if(this.data.x !== undefined){
                     this.dataEnableFlag += "x";
                 }
@@ -202,7 +256,7 @@ if(!window.console){
                 }
             },
 
-            setVal: function(){
+            _setVal: function(){
                 var self = this;
 
                 switch(this.dataEnableFlag){
@@ -214,16 +268,16 @@ if(!window.console){
                         break;
                     case "x":
                         this.$ele.each(function(){
-                            var yVal = $(this).bgPos().y;
-                            self.posPoint = self.data.x + "px " + yVal;
+                            self.data.y = $(this).bgPos().y;
+                            this.posPoint = this.data.x + " " + this.data.y;
 
                             $(this).css("backgroundPosition", self.posPoint);
                         });
                         break;
                     case "y":
                         this.$ele.each(function(){
-                            var xVal = $(this).bgPos().x;
-                            self.posPoint = xVal + " " + self.data.y + "px";
+                            self.data.x = $(this).bgPos().x;
+                            this.posPoint = this.data.x + " " + this.data.y;
 
                             $(this).css("backgroundPosition", self.posPoint);
                         });
@@ -231,25 +285,29 @@ if(!window.console){
                 }
             },
 
-            run: function(data, $ele){
+            run: function($ele, data){
                 this.$ele = $ele;
                 this.data = data;
                 this.dataEnableFlag = "";
 
-                this.dataAddUnit();
-                this.judgeXY();
-                this.setVal();
+                this._dataAddUnit();
+                this._judgeXY();
+                this._setVal();
             }
         }
 
     };
 
+    /*----------------------------------------------------------------------
+        $.fnにbgPosを追加してjQueryプラグインとして使えるようにする
+        $(ele).bgPos() or $(ele).bgPos({ x: Number, y: Number });
+    ----------------------------------------------------------------------*/
     $.fn.bgPos = function(data){
 
         if(arguments.length === 0){
             return bgPosMethods.get.run(this);
         }else{
-            bgPosMethods.set.run(data, this);
+            bgPosMethods.set.run(this, data);
             return this;
         }
 
@@ -258,76 +316,120 @@ if(!window.console){
     /*----------------------------------------------------------------------
         スムーススクロール
         $(ele).scrollAnchors({
-            duration [int]: 【スクロール速度】,
+            speed [int]: 【スクロール速度】,
             easing [string]: 【イージング関数】
         });
     ----------------------------------------------------------------------*/
-    $.fn.scrollAnchors = function(options){
+    $.ScrollAnchors = function(element, options){
+        this.settings = $.extend({}, this.settings, options);
+        this.$element = $(element);
 
-        if(!$.easing.easeInOutQuart)
-            $.easing.easeInOutCirc = function(e,f,a,h,g){if((f/=g/2)<1){return -h/2*(Math.sqrt(1-f*f)-1)+a}return h/2*(Math.sqrt(1-(f-=2)*f)+1)+a};
+        this.$element.click($.proxy(this.clickEvent, this));
+    };
 
-        var settings = {
-            duration: "normal",
-            easing: "easeInOutCirc"
-        };
+    $.ScrollAnchors.prototype = {
+        settings: {
+            speed  : "normal",
+            easing : "easeInOutCirc"
+        },
 
-        if (options)
-            $.extend(settings, options);
+        targetOffset : null,
+        targetTop    : null,
 
-        return $(this).click(function(eve) {
-            eve.preventDefault();
+        $element     : null,
 
-            var targetOffset = $("#" + this.hash.slice(1)).offset(),
-                targetTop = targetOffset.top;
+        clickEvent: function(ev){
+            ev.preventDefault();
+
+            this.targetOffset = $("#" + this.$element[0].hash.slice(1)).offset();
+            this.targetTop    = this.targetOffset.top;
+            
+            this.scrollAnimate();
+        },
+
+        scrollAnimate: function(){
 
             $("html, body").animate({
-                scrollTop: targetTop
-            }, settings.duration, settings.easing, function(){
+                scrollTop: this.targetTop
+            }, this.settings.speed, this.settings.easing, function(){
                 if($.iOSJudge())
                     $(window).trigger("scroll");
             });
+            
+        }
+    };
 
+    $.fn.scrollAnchors = function(options){
+
+        return this.each(function(){
+           new $.ScrollAnchors(this, options);
         });
-    }
+        
+    };
+
+    // easeInOutQuartが存在しない場合設定する。
+    if(!$.easing.easeInOutQuart)
+        $.easing.easeInOutCirc = function(e,f,a,h,g){if((f/=g/2)<1){return -h/2*(Math.sqrt(1-f*f)-1)+a}return h/2*(Math.sqrt(1-(f-=2)*f)+1)+a};
 
     /*----------------------------------------------------------------------
         ロールオーバーやクリックなどのイベントでの画像変更(_onを画像末尾に付与)
-        hoverとして使う場合：$(ele).onImgChange("mouseenter mouseleave");
+        $(ele).onImgChange({
+            setEvent [string]: 【イベント名】,
+            suffix [string]: 【接尾子】
+        });
     ----------------------------------------------------------------------*/
-    $.fn.onImgChange = function(eventType){
+    $.OnImgChange = function(element, options){
+        this.settings = $.extend({}, this.settings, options);
+        this.$element = $(element);
+
+        this._init();
+    };
+
+    $.OnImgChange.prototype = {
+
+        settings: {
+            setEvent : "mouseenter mouseleave",
+            suffix   : "_on"            
+        },
+
+        $element  : null,
+
+        imgChache : new Image(),
+        imgSrc    : null,
+        imgOnSrc  : null,
+        fileType  : null,
+
+        matchRe   : null,
+
+        _setImgChache: function(){                  
+            this.imgSrc   = this.$element.attr("src");
+            this.fileType = this.imgSrc.substring(this.imgSrc.lastIndexOf("."), this.imgSrc.length);
+            this.imgOnSrc = this.imgSrc.replace(this.fileType, this.settings.suffix + this.fileType);
+
+            this.imgChache.src = this.imgOnSrc;
+            this.matchRe       = new RegExp(this.imgOnSrc + "$");
+        },
+
+        _init: function(){
+
+            this.$element.on(this.settings.setEvent, $.proxy(this.setEvent, this));
+            this._setImgChache();
+            
+        },
+
+        setEvent: function(){
+            if(this.$element.attr("src").match(this.matchRe)){
+                this.$element.attr("src", this.imgSrc);
+            }else{
+                this.$element.attr("src", this.imgOnSrc);
+            }            
+        }
+    };
+
+    $.fn.onImgChange = function(options){
 
         return this.each(function(){
-
-            var preloadImgMethod = {
-
-                chache: new Image(),
-                src: null,
-                onSrc: null,
-                fileType: null,
-
-                run: function(ele){                  
-                    this.src = $(ele).attr("src");
-                    this.fileType = this.src.substring(this.src.lastIndexOf("."), this.src.length);
-                    this.onSrc = this.src.replace(this.fileType, "_on" + this.fileType);
-
-                    this.chache = this.onSrc;
-                }
-
-            };
-
-            $(this).on(eventType, function(){
-                var re = new RegExp("_on" + preloadImgMethod.fileType + "$");
-
-                if($(this).attr("src").match(re)){
-                    $(this).attr("src", preloadImgMethod.src);
-                }else{
-                    $(this).attr("src", preloadImgMethod.onSrc);
-                }
-            });
-
-            preloadImgMethod.run(this);
-
+            new $.OnImgChange(this, options);
         });
 
     };
@@ -340,14 +442,17 @@ if(!window.console){
 
         return this.each(function(){
             var $childrenEle = $(this).children(),
-                $parentHeight = $(this).height();
+                parentHeight = $(this).height();
 
             $childrenEle.each(function(){
-                var $this = $(this),
-                    paddingTB = $this.innerHeight() - $this.height();
+                var $childEle = $(this),
+                    childInnerHeight = $childEle.innerHeight(),
+                    paddingTB = childInnerHeight - $childEle.height();
 
-                if($parentHeight > $(this).innerHeight()){
-                    $(this).height($parentHeight - paddingTB);
+                // 親要素の高さと子要素の内側の高さを比較して、
+                // 親要素の方が高ければ子要素を親要素の内側の高さと同じにする
+                if(parentHeight > childInnerHeight){
+                    $childEle.height(parentHeight - paddingTB);
                 }
 
             });
@@ -356,16 +461,45 @@ if(!window.console){
 
     };
 
+    /*--------------------------------------------------------------------------------------
+        文字列を文字コードにして配列として返す
+    --------------------------------------------------------------------------------------*/
+    $.returnCharCodeArray = function(convertString){
+        var charCodeStringArray = [];
+
+        $.each(convertString, function(val){
+            charCodeStringArray.push(convertString.charCodeAt(val));
+        });
+
+        return charCodeStringArray;
+    };
+
+    /*--------------------------------------------------------------------------------------
+        文字コードの配列にしたメールアドレスを要素のhrefに設定する
+    --------------------------------------------------------------------------------------*/
+    $.fn.setRevertMailAddressLink = function(charCodeMailAddress, textInsertFlag){
+        var mailAddress = "";
+
+        $.each(charCodeMailAddress, function(i, val){
+            mailAddress += String.fromCharCode(val);
+        });
+        
+        this.attr("href", "mai" + "lto:" + mailAddress);
+
+        if(textInsertFlag)
+            this.text(mailAddress);
+    };
+
 })(jQuery);
 
 /*----------------------------------------------------------------------
     URLによって実行するスクリプトを切り分ける(カヤック製)
 
-        各URLごとに実行するスクリプトを設定する。
-        dispatcher(/url/, function());
+    1. 各URLごとに実行するスクリプトを設定する。
+       dispatcher(/url/, function());
 
-        location.pathnameを最後に突っ込む。
-        dispatcher(location.pathname);
+    2. location.pathnameを最後に突っ込む。
+       dispatcher(location.pathname);
 
 ----------------------------------------------------------------------*/
 function dispatcher(path, func){
