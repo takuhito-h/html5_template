469c6c6c69b44e3652f00a7d459a2f558c5874db
takuhito-h <bluesbug@gmail.com>
2014-08-07 15:27:16 +0900

    gruntの高速化、簡略化のためjit-gruntを導入

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,9 @@
 module.exports = function(grunt){
+  
+  require("jit-grunt")(grunt, {
+    sprite: "grunt-spritesmith"
+  });
+
 
   grunt.config.init({
 
@@ -131,17 +136,6 @@ module.exports = function(grunt){
 
     grunt.config.set(["imagemin", "pc", "options", "use"], [mozjpeg()]);
   }
-
-  grunt.loadNpmTasks("grunt-styleguide");
-  grunt.loadNpmTasks("grunt-bower-task");
-  grunt.loadNpmTasks("grunt-ect");
-  grunt.loadNpmTasks("grunt-contrib-clean");
-  grunt.loadNpmTasks("grunt-contrib-sass");
-  grunt.loadNpmTasks("grunt-autoprefixer");
-  grunt.loadNpmTasks("grunt-spritesmith");
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-contrib-uglify");
-  grunt.loadNpmTasks("grunt-contrib-imagemin");
 
   grunt.registerTask("build", ["sprite:pc", "sass:pc", "autoprefixer:pc", "uglify:pc"]);
 
