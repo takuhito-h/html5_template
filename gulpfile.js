008388d4e008712e3978c2344b6e5d266e19308d
takuhito-h <bluesbug@gmail.com>
2015-01-03 22:37:45 +0900

    gulpに新たなタスクを追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,29 @@
 var gulp         = require('gulp');
+
+var scsslint     = require('gulp-scss-lint');
 var sass         = require('gulp-ruby-sass');
-var plumber      = require('gulp-plumber');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('sass', function() {
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+/*------------------------------------------------------------------
+    scsslint
+------------------------------------------------------------------*/
+gulp.task('scsslint', function() {
+    return gulp.src([
+        'src/sass/**/*.scss',
+        '!src/sass/layout/_style-normalize.scss',
+        '!src/sass/setup/_sprites.scss',
+    ])
+    .pipe(scsslint());
+});
+
+/*------------------------------------------------------------------
+    css生成
+------------------------------------------------------------------*/
+gulp.task('css', function() {
     return sass('src/sass/', {
         style: 'expanded'
     })
@@ -14,6 +34,36 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('css/'));
 });
 
+/*------------------------------------------------------------------
+    jshint
+------------------------------------------------------------------*/
+gulp.task('jshint', function() {
+    return gulp.src(['src/js/**/*.js', '!src/js/vendor/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+    ;
+});
+
+/*------------------------------------------------------------------
+    js結合
+------------------------------------------------------------------*/
+gulp.task('js', function() {
+    return gulp.src([
+        "src/js/vendor/*.js",
+        "src/js/vendor/mock/*.js",
+        "src/js/modules/dependents/*.js",
+        "src/js/modules/*.js",
+        "src/js/component_manager.js",
+        "src/js/pages/*.js",
+        "src/js/application.js"
+    ])
+        .pipe(concat('script.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('js'))
+    ;
+});
+
 gulp.task('watch', function () {
-    gulp.watch('src/sass/**/*.scss', ['sass']);
+    gulp.watch('src/sass/**/*.scss', ['scsslint', 'css']);
+    gulp.watch('src/js/**/*.js', ['jshint', 'js']);
 });
