8543a7a657fcb176d210713922f13d88a497542b
takuhito-h <bluesbug@gmail.com>
2015-09-04 11:26:01 +0900

    コンポーネント設定をコメントアウト

--- app-sp/src/js/component_manager.js
+++ app-sp/src/js/component_manager.js
@@ -5,91 +5,11 @@
         コンポーネントの設定
     ------------------------------------------------------------------*/
     var COMPONENT_DEFS = {
-        galleryModal : {
+        /*galleryModal : {
             view     : "galleryModal",
             query    : ".js-gallery-modal-wrapper",
             settings : {}
-        },
-        slider : {
-            view     : "slider",
-            query    : ".js-slider",
-            settings : {}
-        },
-        categorySwiper: {
-            view     : "categorySwiper",
-            query    : ".js-category-swiper",
-            settings : {}
-        },
-        offCanvas: {
-            view     : "offCanvas",
-            query    : "body",
-            settings : {}
-        },
-        searchBox: {
-            view     : "searchBox",
-            query    : ".js-search-box",
-            settings : {}
-        },
-        galleryCarousel: {
-            view     : "galleryCarousel",
-            query    : ".js-gallery-carousel",
-            settings : {}
-        },
-        cateLinebreak: {
-            view     : "cateLinebreak",
-            query    : ".l-contents",
-            settings : {}
-        },
-        articleTab: {
-            view     : "articleTab",
-            query    : ".js-article-tab-box",
-            settings : {}
-        },
-        sliderOpacity: {
-            view     : "sliderOpacity",
-            query    : ".js-sliderOpacity-box",
-            settings : {}
-        },
-        photoVisual: {
-            view     : "photoVisual",
-            query    : ".js-photo-box",
-            settings : {}
-        },
-        languageMode: {
-            view     : "languageMode",
-            query    : ".box-brand-index-flagship",
-            settings : {}
-        },
-        heightFix : {
-            view     : "heightFix",
-            query    : ".l-wrapper",
-            settings : {}
-        },
-        sliderArticle : {
-            view     : "sliderArticle",
-            query    : ".entry",
-            settings : {}
-        },
-        shortcodeStyling : {
-            view     : "shortcodeStyling",
-            query    : ".box-shortcode",
-            settings : {}
-        },
-        smoothScroll: {
-            view     : "smoothScroll",
-            query    : ".l-wrapper",
-            settings : {}
-        },
-        pageinnerScroll : {
-            view     : "pageinnerScroll",
-            query    : ".js-contents-shoplist",
-            settings : {}
-        },
-        "lazyload" : {
-            view     : "lazyload",
-            query    : ".js-lazyload",
-            settings : {}
-        }
+        }*/
     };
 
     /*------------------------------------------------------------------
