b2f3a2307103d8524e8645bab3268c767eb84409
takuhito-h <bluesbug@gmail.com>
2018-10-16 15:12:27 +0900

    [add]スムーススクロールを実装

---
+++ app-pc/src/js/view/smoothScroll.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import SmoothScrollJs from "smooth-scroll";
+
+const SmoothScroll = class{
+    constructor(element, setting) {
+        const mySmoothScroll = new SmoothScrollJs('a[href*="#"]');
+    }
+};
+
+module.exports = SmoothScroll;
