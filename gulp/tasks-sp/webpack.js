8cec7384a8772546bcc1b062e2aee0cb1374e4cf
takuhito-h <bluesbug@gmail.com>
2019-06-12 01:29:49 +0900

    [delete]gulp-uglifyを削除

--- gulp/tasks-sp/webpack.js
+++ gulp/tasks-sp/webpack.js
@@ -7,7 +7,6 @@ import gulp from 'gulp';
 import gutil from 'gulp-util';
 import webpack from 'webpack';
 import webpackStream from 'webpack-stream';
-import uglify from 'gulp-uglify';
 import setting from '../setting-sp/webpack.js';
 
 const isRelease = gutil.env.release ? gutil.env.release : false;
