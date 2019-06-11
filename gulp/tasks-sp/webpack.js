75023e1fa78b0b65a22467a479f6487fb26bf800
takuhito-h <bluesbug@gmail.com>
2019-06-12 01:35:37 +0900

    [delete]gulp-utilを削除

--- gulp/tasks-sp/webpack.js
+++ gulp/tasks-sp/webpack.js
@@ -4,12 +4,9 @@
 
 ------------------------------------------------------------------------------------------------*/
 import gulp from 'gulp';
-import gutil from 'gulp-util';
 import webpack from 'webpack';
 import webpackStream from 'webpack-stream';
 import setting from '../setting-sp/webpack.js';
-
-const isRelease = gutil.env.release ? gutil.env.release : false;
 
 /*------------------------------------------------------------------
     task
