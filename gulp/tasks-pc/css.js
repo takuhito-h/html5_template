3eee4efb6ac5195387a55c3c2828e0faa2ce9317
takuhito-h <bluesbug@gmail.com>
2019-06-29 12:12:42 +0900

    [modi]gulp-sourcemapsをgulp4の組み込みサポートに変更

--- gulp/tasks-pc/css.js
+++ gulp/tasks-pc/css.js
@@ -8,7 +8,6 @@ import gutil from 'gulp-util';
 import sass from 'gulp-sass';
 import sassGlob from 'gulp-sass-glob';
 import cssnext from 'gulp-cssnext';
-import sourcemaps from 'gulp-sourcemaps';
 import setting from '../setting-pc/css.js';
 
 const isRelease = gutil.env.release ? gutil.env.release : false;
@@ -18,13 +17,11 @@ const isRelease = gutil.env.release ? gutil.env.release : false;
 ------------------------------------------------------------------*/
 gulp.task('css:pc', function() {
 
-    return gulp.src(setting.src)
-        .pipe(sourcemaps.init())
-            .pipe(sassGlob())
-            .pipe(sass(setting.sass).on('error', sass.logError))
-            .pipe(cssnext(setting.cssnext))
-        .pipe(isRelease ? gutil.noop() : sourcemaps.write('./'))
-            .pipe(gulp.dest(setting.dest))
+    return gulp.src(setting.src, { sourcemaps: true })
+        .pipe(sassGlob())
+        .pipe(sass(setting.sass).on('error', sass.logError))
+        .pipe(cssnext(setting.cssnext))
+        .pipe(gulp.dest(setting.dest, { sourcemaps: './' }))
     ;
 
 });
