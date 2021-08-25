fb3a077f5cdf67128fbc74bf3d854ab49db0d74a
takuhito-h <bluesbug@gmail.com>
2021-08-25 13:31:32 +0900

    [delete]不要な記述を削除

--- gulp/setting-sp/watch.js
+++ gulp/setting-sp/watch.js
@@ -4,6 +4,5 @@ export default {
     "nunjucks" : path.src + "/html/**/*.njk",
     "iconfont" : path.src + "/font/*.svg",
     "css"      : path.src + "/css/**/**/*.scss",
-    "js"       : [path.src + "/js/**/*.js", path.ignore_src + "/js/jst.js"],
-    "jst"      : path.src + "/js/template/*.html.tmpl"
+    "js"       : [path.src + "/js/**/*.js"]
 };
