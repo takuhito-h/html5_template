e587ef78bcf6ccfc892c386fbccc4b5c8de6be3e
takuhito-h <bluesbug@gmail.com>
2021-08-25 13:34:44 +0900

    [modi]gulp-sassをバージョンアップ

--- gulp/tasks-pc/css.js
+++ gulp/tasks-pc/css.js
@@ -5,12 +5,14 @@
 ------------------------------------------------------------------------------------------------*/
 import gulp from 'gulp';
 import gutil from 'gulp-util';
-import sass from 'gulp-sass';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
 import sassGlob from 'gulp-sass-glob';
 import cssnext from 'gulp-cssnext';
 import setting from '../setting-pc/css.js';
 
 const destSourceMaps = gutil.env.release ? false : true;
+const sass = gulpSass(dartSass);
 
 /*------------------------------------------------------------------
     task
