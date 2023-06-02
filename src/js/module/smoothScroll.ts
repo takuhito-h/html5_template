1cd24b46cab407b32805b273c6f4e950c22f4803
takuhito-h <bluesbug@gmail.com>
2023-06-02 21:49:39 +0900

    [delete]不要なパッケージを削除

--- src/js/module/smoothScroll.ts
+++
@@ -1,7 +0,0 @@
-import SmoothScrollJs from "smooth-scroll";
-
-export default class{
-    constructor(element, setting) {
-        const mySmoothScroll = new SmoothScrollJs('a[href*="#"]');
-    }
-}
