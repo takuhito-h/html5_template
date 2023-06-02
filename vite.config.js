7b3c932c3449bd64981189ef53d17e3ebedfe65b
takuhito-h <bluesbug@gmail.com>
2023-06-02 17:59:43 +0900

    [add]nunjucksをviteに導入

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
 import { defineConfig } from 'vite';
+import nunjucks from '@vituum/vite-plugin-nunjucks';
 import sassGlobImports from 'vite-plugin-sass-glob-import';
+import fastGlob from 'fast-glob';
 import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     root: path.join(__dirname, 'src'),
+    publicDir: path.join(__dirname, 'copy'),
     plugins: [
-        sassGlobImports()
-    ]
+        sassGlobImports(),
+        nunjucks({
+            filetypes: {
+                html: /.(html|njk|njk.html)$/,
+            },
+        }),
+    ],
+    build: {
+        emptyOutDir: true,
+        outDir: path.join(__dirname, 'public'),
+        rollupOptions: {
+            input: fastGlob.sync(['src/*.html', 'src/pages/**/*.html'], { dot: false }),
+        },
+    },
 });
