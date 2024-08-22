a352a21366caaf5704c6669472d9c886e1fd6ae2
takuhito-h <bluesbug@gmail.com>
2024-08-22 15:35:38 +0900

    [delete]不要なファイルを削除

--- src/js/module/lazyLoad.ts
+++
@@ -1,9 +0,0 @@
-import LazyLoadJs from "vanilla-lazyload";
-
-export default class{
-    constructor(element:string, setting) {
-        const myLazyLoad = new LazyLoadJs({
-            container: element
-        });
-    }
-}
