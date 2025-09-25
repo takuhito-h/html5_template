82e0c000bfe9978b92bf4927d6cffd068031efcb
takuhito-h <bluesbug@gmail.com>
2025-09-25 14:40:41 +0900

    [add]パッケージをアップデート

--- postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
-
-module.exports = {
-    plugins: [
-        purgecss.default({
-            content: ['./src/**/*.html', './src/**/*.njk']
-        })
-    ]
-}
